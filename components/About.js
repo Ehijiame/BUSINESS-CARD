ae7db14734865a6ab0e45cb9e2f16c64c290f29b
Ehijiame <nonyeb2000@gmail.com>
2024-12-10 21:45:37 +0100

    first commit

---
+++ components/About.js
@@ -0,0 +1,19 @@
+
+ import React from "react"
+
+export default function Info (){
+    return(
+        <div className="info">
+          <img src="/images/image1.png"/>
+          <h3> Alegbe Blessing </h3>
+          <h4> Frontend Developer </h4>
+          <h5> nonyeb200@gmail.com </h5>
+          <p> 
+          <span className="firstspan">  <i class="fa-solid fa-envelope"></i> Email </span>
+           <span className="secondspan"> <i class="fa-brands fa-linkedin"></i> LinkedIn </span>
+           </p>
+        
+        </div>
+    )
+  
+}
---
+++ components/Interests.js
@@ -0,0 +1,18 @@
+
+ import React from "react"
+
+export default function Footer(){
+    return(
+        <div className="footer">
+    
+        <ul>
+        <li><i class="fa-brands fa-square-twitter"></i> </li>
+         <li> <i class="fa-brands fa-square-facebook"></i> </li>
+          <li> <i class="fa-brands fa-square-instagram"></i> </li>
+           <li> <i class="fa-brands fa-linkedin"></i> </li>
+            <li><i class="fa-brands fa-square-github"></i> </li>
+        </ul>
+  
+        </div>
+    )
+}
